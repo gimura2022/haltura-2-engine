379ed7cb68da4cfe4de9cea05039f91732015933
gimura2022 <gimura0001@gmail.com>
2024-11-13 22:22:19 +0300

    transformations separated to eng_transform.c/h file

--- src/eng_math.c
+++ src/eng_math.c
@@ -3,67 +3,8 @@
 
 #include <eng_math.h>
 
-void mat4x4_create_prespective_matrix(const double fov, const double near, const double far, mat4x4* mat)
-{
-	math_mat4x4__create_zero(mat);
 
-	const double scale = 1 / tan(fov * 0.5 * M_PI / 180);
 
-	mat->x.x = scale;
-	mat->y.y = scale;
-	mat->z.z = -far / (far - near);
-	mat->w.z = -far * near / (far - near);
-	mat->z.w = -1;
-	mat->w.w = 0;
-}
-
-void mat4x4_create_rotation_matrix(const double theta, const vec4* to, mat4x4* mat)
-{
-	math_mat4x4__create_zero(mat);
-
-	const double sint = sin(theta);
-	const double cost = cos(theta);
-
-	const double x = to->x;
-	const double y = to->y;
-	const double z = to->z;
-
-	mat->x.x = x * x * (1 - cost) + cost;
-	mat->x.y = y * x * (1 - cost) - z * sint;
-	mat->x.z = z * x * (1 - cost) + y * sint;
-	mat->x.w = 0;
-
-	mat->y.x = x * y * (1 - cost) + z * sint;
-	mat->y.y = y * y * (1 - cost) + cost;
-	mat->y.z = z * y * (1 - cost) - x * sint;
-	mat->y.w = 0;
-
-	mat->z.x = x * z * (1 - cost) - y * sint;
-	mat->z.y = y * z * (1 - cost) + x * sint;
-	mat->z.z = z * z * (1 - cost) + cost;
-	mat->z.w = 0;
-
-	mat->w.w = 1;
-}
-
-void mat4x4_create_size_matrix(const vec4* size, mat4x4* mat)
-{
-	math_mat4x4__create_zero(mat);
-
-	mat->x.x = size->x;
-	mat->y.y = size->y;
-	mat->z.z = size->z;
-	mat->w.w = size->w;
-}
-
-void mat4x4_create_transform_matrix(const vec4* pos, mat4x4* mat)
-{
-	math_mat4x4__create_zero(mat);
-
-	mat->x.w = pos->x;
-	mat->y.w = pos->y;
-	mat->z.w = pos->z;
-}
 
 void math_mat4x4__apply_to_vec4(const mat4x4* mat, vec4* vec)
 {
